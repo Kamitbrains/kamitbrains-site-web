ba093f5d07b2825d5feb815c52b54cafdfed3ff7
NKUMBE AURELIEN <nkaurelien@gmail.com>
2018-02-28 16:51:28 +0100

    ajout du packet nodemon

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var diskdb = require('diskdb');
+var expressMail = require('express-mail');
 var app = express();
-
-db = diskdb.connect('datastorage', ['equipes']);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +11,24 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+db = diskdb.connect('datastorage', ['equipes']);
+
+expressMail.extend(app, {
+  transport: 'SMTP',
+  config: {
+    service: 'Gmail',
+    auth: {
+      user: 'kamitbrains@gmail.com',
+      pass: 'Aurelio_81g_n3aa_1993'
+    }
+  },
+  defaults: {
+    from: 'kamitbrains@gmail.com'
+  }
+});
+
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -19,6 +36,24 @@ app.get('/', function(request, response) {
 
 app.post('/sendmail', function(request, response) {
   
+// Setup email data. 
+var mailOptions = {
+  to: 'bar@blurdybloop.com',
+  subject: 'Contact from Kamitbrains website ✔',
+  locals: {
+    title: 'Hello Admin',
+    message: 'Welcome to my website'
+  }
+}
+ 
+// Send email. 
+app.send('mail', mailOptions, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Message sent: ' + response.message);
+  }
+});
 });
 
 app.listen(app.get('port'), function() {
