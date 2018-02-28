d3e1be236f71d18c034a13adf92e9b9c9f7ab313
NKUMBE AURELIEN <nkaurelien@gmail.com>
2018-02-28 17:28:42 +0100

    sd

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
-var diskdb = require('diskdb');
-//var expressMail = require('express-mail');
+var diskdb = require('diskdb');var expressMail = require('express-mail');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,20 +13,6 @@ app.set('view engine', 'ejs');
 
 db = diskdb.connect('datastorage', ['equipes']);
 
-expressMail.extend(app, {
-  transport: 'SMTP',
-  config: {
-    service: 'Gmail',
-    auth: {
-      user: 'kamitbrains@gmail.com',
-      pass: 'Aurelio_81g_n3aa_1993'
-    }
-  },
-  defaults: {
-    from: 'kamitbrains@gmail.com'
-  }
-});
-
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -36,24 +21,39 @@ app.get('/', function(request, response) {
 
 app.post('/sendmail', function(request, response) {
   
-// Setup email data. 
-var mailOptions = {
-  to: 'bar@blurdybloop.com',
-  subject: 'Contact from Kamitbrains website ✔',
-  locals: {
-    title: 'Hello Admin',
-    message: 'Welcome to my website'
-  }
-}
- 
-// Send email. 
-app.send('mail', mailOptions, function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Message sent: ' + response.message);
-  }
-});
+  // expressMail.extend(app, {
+  //   transport: 'SMTP',
+  //   config: {
+  //     service: 'Gmail',
+  //     auth: {
+  //       user: 'kamitbrains@gmail.com',
+  //       pass: 'Aurelio_81g_n3aa_1993'
+  //     }
+  //   },
+  //   defaults: {
+  //     from: 'kamitbrains@gmail.com'
+  //   }
+  // });
+
+  // // Setup email data. 
+  // var mailOptions = {
+  //   to: 'bar@blurdybloop.com',
+  //   subject: 'Contact from Kamitbrains website ✔',
+  //   locals: {
+  //     title: 'Hello Admin',
+  //     message: 'Welcome to my website'
+  //   }
+  // }
+  
+  // // Send email. 
+  // app.send('mail', mailOptions, function (error, response) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('Message sent: ' + response.message);
+  //   }
+  // });
+
 });
 
 app.listen(app.get('port'), function() {
